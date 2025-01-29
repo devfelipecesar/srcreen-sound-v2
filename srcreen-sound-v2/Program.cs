342a0cc2d97a147abaa6ebfe39e7931130065aaa
devfelipecesar <devfelipecesar@gmail.com>
2025-01-29 14:37:57 -0300

    Primeiro commit

--- srcreen-sound-v2/Program.cs
+++ srcreen-sound-v2/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿Musica musica1 = new Musica(); //criação de objeto
+musica1.nome = "Roxane";
+musica1.artista = "The Police";
+
+Console.WriteLine($"Nome da musica: {musica1.nome}");
+Console.WriteLine($"Nome da banda: {musica1.artista}");
+
+Musica musica2 = new Musica();
+musica2.nome = "Vertigo";
+musica2.artista = "U2";
